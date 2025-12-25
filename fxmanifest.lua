e76c3be9b3d35d80ed3b885249c89589ff3c7d18
LordOfTheCorgis <evanvoisel@yahoo.com>
2025-12-25 01:17:05 -0600

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'LordOfTheCorgis'
+description 'Loads the Doomsday Bunker, and other interiors, from the Doomsday Heist Series'
+
+client_scripts {
+    'client.lua'
+}
